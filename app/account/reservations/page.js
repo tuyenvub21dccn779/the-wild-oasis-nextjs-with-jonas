2ef4247ee76af48bd4e04a4aff9fdaed40c5bba7
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-05-27 15:12:32 +0700

    012 Adding a Nested Layout

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,5 +1,9 @@
 import ReservationCard from "@/app/_components/ReservationCard";
 
+export const metadata = {
+  title: "Reservation",
+};
+
 export default function Page() {
   // CHANGE
   const bookings = [];
--- app/layout.js
+++ app/layout.js
@@ -25,8 +25,8 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col `}
       >
         <Header />
-        <div className="flex-1 px-8 py-12 relative">
-          <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 relative grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
