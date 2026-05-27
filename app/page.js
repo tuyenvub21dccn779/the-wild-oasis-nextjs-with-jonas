710dccf9c9c58113fa0e4e9f245e64024754289a
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-05-27 09:30:31 +0700

    012 Creating a Layout

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ import Navigation from "./components/Navigation";
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>The Wild Oasis. Welcome to paradise.</h1>
     </div>
   );
