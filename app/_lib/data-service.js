34f98a266a1c3ea6bf78ba619807348a567b321f
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-05-27 16:43:02 +0700

    006 Streaming UI With Suspense Cabin List

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -41,6 +41,9 @@ export const getCabins = async function () {
     .select("id, name, maxCapacity, regularPrice, discount, image")
     .order("name");
 
+  // For testing
+  // await new Promise((res) => setTimeout(res, 1000));
+
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
