b4730c9da05add4a2aa0975995cf50c4f38aa625
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-05-29 10:09:25 +0700

    008 Creating a New Guest on First Sign In

--- app/account/page.js
+++ app/account/page.js
@@ -6,10 +6,13 @@ export const metadata = {
 
 export default async function Page() {
   const session = await auth();
+  console.log(session);
+
+  const firstName = session.user.name.split(" ").at(0);
 
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, {session.user.name}
+      Welcome, {firstName}
     </h2>
   );
 }
