317cb900cf2072857c7058e52909dddded58383e
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-05-30 16:56:32 +0700

    008 Another Loading Indicator The useTransition Hook

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -30,12 +30,12 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile");
 }
 
-export async function deleteReservation({ bookingId }) {
+export async function deleteReservation(bookingId) {
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
 
   const guestBookings = await getBookings(session.user.guestId);
-  const guestBookingIds = getBookings.map((booking) => booking.id);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
 
   if (!guestBookingIds.includes(bookingId))
     throw new Error("You are not allow to delete this booking");
