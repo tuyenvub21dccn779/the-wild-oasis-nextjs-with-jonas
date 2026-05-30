59070ede572170b55d5d194e79037ed9519f2c27
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-05-30 22:52:13 +0700

    011 Back to the Cabin Page Finishing the Date Selector

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -23,10 +23,6 @@ function ReservationForm({ cabin, user }) {
         </div>
       </div>
 
-      <p>
-        {String(range.from)} to {String(range.to)}
-      </p>
-
       <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
         <div className="space-y-2">
           <label htmlFor="numGuests">How many guests?</label>
