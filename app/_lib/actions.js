4b6c9744d56cb43a2a0eac596f2390040b7b0234
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-05-30 18:12:55 +0700

    009 CHALLENGE #1 Updating a Reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -31,32 +32,43 @@ export async function updateGuest(formData) {
 }
 
 export async function updateBooking(formData) {
+  // 1) Authentication
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
 
+  // 2) Authorization
   const guestBookings = await getBookings(session.user.guestId);
   const guestBookingIds = guestBookings.map((booking) => booking.id);
 
-  const bookingId = Number(formData.get("reservationId"));
+  const bookingId = Number(formData.get("bookingId"));
 
   if (!guestBookingIds.includes(bookingId))
     throw new Error("You are not allow to update this booking");
 
-  const numGuests = Number(formData.get("numGuests"));
-  const observations = formData.get("observations");
-  const updateData = { numGuests, observations };
+  // 3) Building update data
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
 
+  // 4) Mutation
   const { error } = await supabase
     .from("bookings")
     .update(updateData)
     .eq("id", bookingId);
 
+  // 5) Error handling
   if (error) {
     console.error(error);
     throw new Error("Booking could not be updated");
   }
 
+  // 6) Revalidation
+  revalidatePath("/account/reservations");
   revalidatePath(`/account/reservations/edit/${bookingId}`);
+
+  // 7) Redirecting
+  redirect("/account/reservations");
 }
 
 export async function deleteReservation(bookingId) {
--- app/account/reservations/edit/[bookingId]/page.js
+++ app/account/reservations/edit/[bookingId]/page.js
@@ -1,27 +1,30 @@
+import SubmitButton from "@/app/_components/SubmitButton";
 import { updateBooking } from "@/app/_lib/actions";
 import { getBooking, getCabin, getSettings } from "@/app/_lib/data-service";
 
 export async function generateMetadata({ params }) {
-  return { title: `Edit Reservation ${params.reservationId}` };
+  return { title: `Edit Reservation ${params.bookingId}` };
 }
 
 export default async function Page({ params }) {
   // CHANGE
-  const { reservationId } = params;
-  const reservation = await getBooking(params.reservationId);
-  const { maxCapacity } = await getCabin(reservation.cabinId);
+  const { bookingId } = params;
+  const { numGuests, observations, cabinId } = await getBooking(
+    params.bookingId,
+  );
+  const { maxCapacity } = await getCabin(cabinId);
 
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Edit Reservation #{reservationId}
+        Edit Reservation #{bookingId}
       </h2>
 
       <form
         action={updateBooking}
         className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
       >
-        <input type="hidden" name="reservationId" value={reservationId} />
+        <input type="hidden" name="bookingId" value={bookingId} />
         <div className="space-y-2">
           <label htmlFor="numGuests">How many guests?</label>
           <select
@@ -29,7 +32,7 @@ export default async function Page({ params }) {
             id="numGuests"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
             required
-            defaultValue={reservation.numGuests}
+            defaultValue={numGuests}
           >
             <option value="" key="">
               Select number of guests...
@@ -50,14 +53,14 @@ export default async function Page({ params }) {
             name="observations"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
           >
-            {reservation.observations}
+            {observations}
           </textarea>
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+          <SubmitButton pendingLabel="Updating...">
             Update reservation
-          </button>
+          </SubmitButton>
         </div>
       </form>
     </div>
